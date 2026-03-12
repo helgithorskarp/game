eb53d832ad90ae649f3d3b29abea64ce8054a5e8
alexanderms24 <alexanderms24@ru.is>
2026-03-12 23:14:08 +0000

    Finalize fight loop

--- fight_loop.h
+++ fight_loop.h
@@ -1,4 +1,6 @@
 #include "Combatant.h"
 #include "User.h"
 
+void print_combat_result(string result);
+void print_combat_header(User &userPlayer, Combatant &monster);
 bool fight_loop(User &userPlayer, Combatant &monster);
